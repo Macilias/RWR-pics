1575c299bff05960d71ec953b523a9924cddcc09
Maciek Niemczyk <Maciek.Niemczyk@vitruam.de>
2019-08-07 11:36:16 +0200

    init 2 async
     init 3
    
     init 4 V1 done
    
     init 5 V2 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import axios from 'axios';
-import SearchBar from "./SearchBar";
+import unsplash from '../api/unsplash';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
 
+	state = { images: [], total: 0 };
 
-	onSearchSubmit(term) {
-		const api = 'https://api.unsplash.com/';
-		axios.get(`${ api }search/photos`, {
-			params: {
-				query: term
-			},
-			headers: {
-				Authorization: 'Client-ID 956353f9d322a9f3cb3e61708da66ed2a6ed4d764f9aaf12db11892f439b8ac5'
-			}
-		}).then((response) => {
-			console.log(response);
+	onSearchSubmit = async (term) => {
+		const response = await unsplash.get('/search/photos', {
+			params: { query: term }
 		});
-	}
+
+		this.setState({ images: response.data.results, total: response.data.total });
+	};
 
 	render() {
 		return (
 			<div className="ui container" style={ { marginTop: '10px' } }>
 				<SearchBar onSubmit={ this.onSearchSubmit }/>
+				<ImageList images={this.state.images}/>
 			</div>
 		);
 	}
